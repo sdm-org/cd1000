13b5fa135c0f4f9c9e29ac72f80183bcd1002ce5
Christian Dupuis <cd@atomist.com>
2018-12-20 12:48:15 +0100

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/Cd1000Application.java
@@ -0,0 +1,12 @@
+package com.atomist;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cd1000Application {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(Cd1000Application.class, args);
+	}
+
+}
